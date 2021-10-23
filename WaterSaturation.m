21f11b1eca26cab2847b723424fdb22d515b1d63
akinyeleolaitan <49063579+akinyeleolaitan@users.noreply.github.com>
2021-10-23 23:54:31 +0100

    Delete WaterSaturation.m

--- WaterSaturation.m
+++
@@ -1,6 +0,0 @@
-function ret = WaterSaturation(Snw, data)
-% This function computes water saturation given normalized water saturation
-
-Siw = data.Siw;
-Sor = data.Sor;
-ret = Siw + Snw.*(1 - Sor - Siw);
